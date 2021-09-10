d2064e411f0e49b22f08e78f62b913753c32ce8a
Jerax <dgjackon01@gmail.com>
2021-09-09 19:20:24 -0700

    skills and navbar changes

---
+++ src/components/Skills.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+function Skills() {
+  return (
+    <Fragment>
+      <div className="skills">
+        <p>
+          Programming languages and frameworks I am proficient with include React, Django, Javascript, Python, HTML and CSS.
+        </p>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Skills;
